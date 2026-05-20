ed920c219bf4ccc430958c69a481f882cdd73141
guilhermeteixeira01 <guilherme.teixeira00@outlook.com>
2026-05-19 22:55:26 -0300

    refactor: simplificar estrutura de roteamento e remover provedores desnecessários

--- GerarEmbed/main.jsx
+++ GerarEmbed/main.jsx
@@ -1,28 +1,17 @@
 // src/main.jsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import './App.css';
-import App from './App';
-import NotFoundPage from './NotFoundPage';
-import { AuthProvider } from './context/AuthContext';
-import { ToastProvider } from './context/ToastContext';
-import { LogProvider } from './context/LogContext';
+import App from './gerarembed.jsx';
+import NotFoundPage from './NotFoundPage.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter basename="/DashBoard/">
-            <AuthProvider>
-                <LogProvider>
-                    <ToastProvider>
-                        <Routes>
-                            <Route path="/" element={<App />} />
-                            <Route path="*" element={<NotFoundPage />} />
-                        </Routes>
-                    </ToastProvider>
-                </LogProvider>
-            </AuthProvider>
+        <BrowserRouter basename="/GerarEmbed/">
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
         </BrowserRouter>
     </StrictMode>
 );
